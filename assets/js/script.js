54fb2c41f2fa7f7588301d6714063b2fc89408e8
odv144 <omar.virili@gmail.com>
2023-05-02 23:36:48 -0300

    agregando slider basico

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+// codigo que permite ejercutarlo recien cuando cargue todo el dom
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Tu código aquí
+//   });
+
+const imag=["armonie_chisiamo1.jpg",
+    "armonie_centro_estetico.jpg",
+    "centro-estetico1.jpg",
+    "punto_vendita_armonie.jpg"];
+var index=0;
+var carrusel= document.getElementById("carrusel");
+var imagen = document.createElement("img");
+
+function slider(){
+    imagen.src="/assets/img/"+imag[index];
+    
+    carrusel.appendChild(imagen);
+    
+    (index<3)?index++:index=0;
+}
+
+setInterval(slider, 2000);
